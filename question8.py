20524c0add9d7bf5cd04103c9b8495f61f122f32
bonnieives <120677106+bonnieives@users.noreply.github.com>
2023-09-20 18:43:17 -0400

    Add files via upload

---
+++ question8.py
@@ -0,0 +1,33 @@
+"""
+
+    ASSIGNMENT #1
+    QUESTION 8
+    AUTHOR: Bonnie Ives de Castro Nunes
+    DATE: September 15th, 2023
+    
+"""
+
+listOne = [3, 6, 9, 12, 15, 18, 21]
+listTwo = [4, 8, 12, 16, 20, 24, 28]
+
+print(" ----- Lists informed ----- ")
+print("List one:", listOne)
+print("List two:", listTwo)
+
+lenOne = len(listOne)
+lenTwo = len(listTwo)
+
+finalList = []
+
+for itemOne in range(0,lenOne):
+
+    if ((itemOne % 2) == 1):
+        finalList.append(listOne[itemOne])
+
+for itemTwo in range(0,lenTwo):
+
+    if ((itemTwo % 2) == 0):
+        finalList.append(listTwo[itemTwo])
+
+print("\n ----- Final list -----")
+print(finalList)
